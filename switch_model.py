b4a3633a7e2aefc41963cbd7fa9eccc976bf044c
Dotzer <dotza65@gmail.com>
2017-01-04 10:33:40 +1100

    minor changes + converging to pep8

--- switch_model.py
+++ switch_model.py
@@ -2,12 +2,12 @@
 import telnetlib
 import sys
 
-qtech_login = [r"Username\(", r"login\:"]
-dlink_login = [r"serName\:"]
-login = qtech_login + dlink_login
-tl_password = [r"assword\(",r"assword\:",r"assWord\:"]
-models = [r'2800-28', r'2850-28', r'2910-52', r'2900-24', r'3000-24', r'3000-26', r'2800-10']
+login = [r"Username\(", r"login\:", r"serName\:"]
+tl_password = [r"assword\(", r"assword\:", r"assWord\:"]
+models = [r'2800-28', r'2850-28', r'2910-52', r'2900-24',
+          r'3000-24', r'3000-26', r'2800-10']
 prompt = [r'\#', r'\>']
+
 
 def log_in(manu):
     tn.write('marvin\n')
@@ -24,11 +24,11 @@ def log_in(manu):
 
 host = sys.argv[1]
 tn = telnetlib.Telnet(host)
-login_out = tn.expect(login,3)
+login_out = tn.expect(login, 3)
 if login_out[0] >= 0 and login_out[0] < 2:
     result = log_in('qtech')
-elif login_out[0] >1:
-    result =  log_in('dlink')
+elif login_out[0] == 2:
+    result = log_in('dlink')
 else:
     print 'Unknown'
 if not result == -1:
